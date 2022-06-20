e264f0eaef00249a861442ddfafecc4f9b473026
santhiya <santhiya30032@gmail.com>
2022-06-20 07:58:05 -0700

    my first react

--- myreactapp/src/App.js
+++ myreactapp/src/App.js
@@ -1,25 +1,143 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './App.js';
+import PriceCard from "./PriceCard";
+
+
 
 function App() {
+  let priceCard =[ 
+    {
+      title: 'Free',
+      Price: 0,
+      features :[
+        {
+          name :"Single User",
+          isEnable :true
+        },
+        {
+          name :"5GB Storage",
+          isEnable :true
+        },
+        {
+          name :"Unlimited Public Projects",
+          isEnable :true
+        },
+        {
+          name :"Community Access",
+          isEnable :true
+        },
+        {
+          name :"Unlimited Private Projects",
+          isEnable : false
+        },
+        {
+          name :"Dedicated Phone Support",
+          isEnable : false
+        },
+        {
+          name :"Free Subdomain",
+          isEnable : false
+        },
+        {
+          name :"Monthly Status Reports",
+          isEnable : false
+        },
+      ]
+
+    },
+    {
+      title: 'Plus',
+      Price: 29,
+      features :[
+        {
+          name : "5 Users",
+          isEnable : true
+        },
+        {
+          name : "50GB Storage",
+          isEnable : true
+        },
+        {
+          name : "Unlimited Public Projects",
+          isEnable : true
+        },
+        {
+          name : "Community Access",
+          isEnable : true
+        },
+        {
+          name : "Unlimited Private Projects",
+          isEnable : true
+        },
+        {
+          name : "Dedicated Phone Support",
+          isEnable : true
+        },
+        {
+          name : "Free Subdomain",
+          isEnable : true
+        },
+        {
+          name : "Monthly Status Reports",
+          isEnable : false
+        },
+      ]
+      
+    },
+    {
+      title: 'Pro',
+      Price: 49,
+      features : [
+        {
+          name : "Unlimited Users",
+          isEnable : true
+        },
+        {
+          name : "150GB Storage",
+          isEnable : true
+        },
+        {
+          name : "Unlimited Public Projects",
+          isEnable : true
+        },
+        {
+          name : "Community Access",
+          isEnable : true
+        },
+        {
+          name : "Unlimited Private Projects",
+          isEnable : true
+        },
+        {
+          name : "Dedicated Phone Support",
+          isEnable : true
+        },
+        {
+          name : "Unlimited Free Subdomains",
+          isEnable : true
+        },
+        {
+          name : "Monthly Status Reports",
+          isEnable : true
+        },
+      ]
+      
+    }
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="pricing py-5">
+       <div className="container">
+       <div className="row">
+      {
+        priceCard.map((card)=>{
+return <PriceCard data={card}></PriceCard>
+
+        })
+      }
+       </div>
+       </div>
+    </section>
+
   );
 }
-
-export default App;
+export default App
---
+++ myreactapp/src/PriceCard.js
@@ -0,0 +1,34 @@
+function PriceCard({ data }) {
+  return (
+    <div className="col-lg-4">
+      <div className="card mb-5 mb-lg-0">
+        <div className="card-body">
+          <h5 className="card-title text-muted text-uppercase text-center">{data.title}</h5>
+          <h6 className="card-price text-center">
+            ${data.Price}<span className="period">/month</span>
+          </h6>
+          <hr />
+          <ul className="fa-ul">
+            {
+              data.features.map((list)=>{
+                return <li className={list.isEnable ? '':'text-muted'}>
+                <span className="fa-li">
+                  <i className="fas fa-check"></i>
+                </span>
+                {list.name}
+                </li>
+              })
+            }
+
+          </ul>
+          <div className="d-grid">
+            <a href="#" className="btn btn-primary text-uppercase">
+              Button
+            </a>
+    </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default PriceCard
